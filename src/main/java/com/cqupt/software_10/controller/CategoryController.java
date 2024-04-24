8af280fb99980b7ae520524a61f106425f369346
山海有灵 <2350836852@qq.com>
2024-04-24 11:18:50 +0800

    V3.0，Li Tan 04 24 10:31 将杨宗庆的代码加入了

--- src/main/java/com/cqupt/software_10/controller/CategoryController.java
+++ src/main/java/com/cqupt/software_10/controller/CategoryController.java
@@ -5,8 +5,11 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.cqupt.software_10.common.Result;
 import com.cqupt.software_10.dao.CategoryMapper;
+import com.cqupt.software_10.dao.TableDescribeMapper;
 import com.cqupt.software_10.entity.Category2Entity;
 import com.cqupt.software_10.entity.CategoryEntity;
+import com.cqupt.software_10.entity.TableDescribeEntity;
+import com.cqupt.software_10.mapper.user.UserMapper;
 import com.cqupt.software_10.service.Category2Service;
 import com.cqupt.software_10.service.CategoryService;
 import com.cqupt.software_10.vo.AddDiseaseVo;
@@ -15,6 +18,8 @@ import com.cqupt.software_10.vo.UpdateDiseaseVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.StringJoiner;
@@ -27,15 +32,29 @@ public class CategoryController {
     CategoryService categoryService;
     @Autowired
     Category2Service category2Service;
+
+    @Autowired
+    TableDescribeMapper tableDescribeMapper;
+
+    @Autowired
+    UserMapper userMapper;
+
     @Autowired
     CategoryMapper categoryMapper;
 
 
     // 获取目录
     @GetMapping("/category")
+    public Result<List<CategoryEntity>> getCatgory(@RequestParam String uid){
+        List<CategoryEntity> list = categoryService.getCategory(uid);
+//        System.out.println(JSON.toJSONString(list));
+        return Result.success("200",list);
+    }
+
+    @GetMapping("/Taskcategory")
     public Result<List<CategoryEntity>> getCatgory(){
-        List<CategoryEntity> list = categoryService.getCategory();
-        System.out.println(JSON.toJSONString(list));
+        List<CategoryEntity> list = categoryService.getTaskCategory();
+//        System.out.println(JSON.toJSONString(list));
         return Result.success("200",list);
     }
 
@@ -57,20 +76,27 @@ public class CategoryController {
 
 
     // 删除一个目录
+    @Transactional
     @GetMapping("/category/remove")
     public Result removeCate(CategoryEntity categoryEntity){
         System.out.println("要删除的目录为："+JSON.toJSONString(categoryEntity));
-        categoryService.removeNode(categoryEntity.getId());
+        if(categoryEntity.getIsLeafs()==0){
+            categoryService.removeNode(categoryEntity.getId());
+        }
+        else {
+            categoryService.removeNode(categoryEntity.getId(),categoryEntity.getLabel());
+            TableDescribeEntity tableDescribeEntity = tableDescribeMapper.selectOne(new QueryWrapper<TableDescribeEntity>().eq("table_id",categoryEntity.getId()));
+            if(tableDescribeEntity.getTableSize()!=0){
+                userMapper.recoveryUpdateUserColumnById(tableDescribeEntity.getUid(),tableDescribeEntity.getTableSize());
+            }
+            tableDescribeMapper.delete(new QueryWrapper<TableDescribeEntity>().eq("table_id",categoryEntity.getId()));
+//            tTableMapper.delete(new QueryWrapper<tTable>().eq("table_name",categoryEntity.getLabel()));
+
+        }
         return Result.success(200,"删除成功");
     }
 
 
-    @GetMapping("/category/getAllDisease")
-    public Result<List<CategoryEntity>> getAllDisease(){
-        List<CategoryEntity> list = categoryService.getAllDisease();
-        System.out.println(JSON.toJSONString(list));
-        return Result.success("200",list);
-    }
     /**
      * 新增检查疾病名是否存在
      */
@@ -106,32 +132,8 @@ public class CategoryController {
     }
 
 
-    @GetMapping("/addParentDisease")
-    public Result addParentDisease(@RequestParam("diseaseName") String diseaseName){
-        System.out.println("name:"+diseaseName);
-        categoryService.addParentDisease(diseaseName);
-        return Result.success(200,"添加成功");
-    }
 
 
-    @PostMapping("/category/addCategory")
-    public Result addCategory(@RequestBody AddDiseaseVo addDiseaseVo){
-        if(categoryService.addCategory(addDiseaseVo)>0){
-//            UserLog userLog = new UserLog(null,Integer.parseInt(addDiseaseVo.getUid()),new Date(),"添加病种"+addDiseaseVo.getFirstDisease(),addDiseaseVo.getUsername());
-//            userLogService.save(userLog);
-            return Result.success("添加病种成功");
-        }else{
-//            UserLog userLog = new UserLog(null,Integer.parseInt(addDiseaseVo.getUid()),new Date(),"添加病种失败",addDiseaseVo.getUsername());
-//            userLogService.save(userLog);
-            return Result.fail("添加病种失败");
-        }
-    }
-    @PostMapping("/category/updateCategory")
-    public Result updateCategory(@RequestBody UpdateDiseaseVo updateDiseaseVo){
-//        UserLog userLog = new UserLog(null,Integer.parseInt(updateDiseaseVo.getUid()),new Date(),"修改病种"+updateDiseaseVo.getOldName()+"为"+updateDiseaseVo.getDiseaseName(),updateDiseaseVo.getUsername());
-//        userLogService.save(userLog);
-        return categoryService.updateCategory(updateDiseaseVo);
-    }
     @PostMapping("/category/deleteCategory")
     public Result deleteCategory(@RequestBody DeleteDiseaseVo deleteDiseaseVo){
         StringJoiner joiner = new StringJoiner(",");
@@ -141,6 +143,103 @@ public class CategoryController {
 //        UserLog userLog = new UserLog(null,Integer.parseInt(deleteDiseaseVo.getUid()),new Date(),"删除病种："+joiner.toString(),deleteDiseaseVo.getUsername());
 //        userLogService.save(userLog);
         categoryService.removeCategorys(deleteDiseaseVo.getDeleteIds());
+        return Result.success("删除成功");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @GetMapping("/addParentDisease")
+    public Result addParentDisease(@RequestParam("diseaseName") String diseaseName){
+        System.out.println("name:"+diseaseName);
+        categoryService.addParentDisease(diseaseName);
+        return Result.success("200",null);
+    }
+
+    @GetMapping("/changeStatus")
+    public Result changeStatus(CategoryEntity categoryEntity){
+        categoryService.changeStatus(categoryEntity);
+        return Result.success(200,"修改成功",null);
+    }
+
+
+    @GetMapping("/inspectionOfIsNotCommon")
+    public Result inspectionOfIsNotCommon(@RequestParam("newname") String name){
+        QueryWrapper<CategoryEntity> wrapper = new QueryWrapper<>();
+        wrapper.eq("is_delete", 0);
+        wrapper.eq("is_common", 0);
+        List<CategoryEntity> list = categoryService.list(wrapper);
+        List<String>  nameList = new ArrayList<>();
+
+        for (CategoryEntity temp :list) {
+            nameList.add(temp.getLabel());
+        }
+        boolean flag = true;
+        for (String  tempName : nameList) {
+            if(tempName.equals(name)) {
+                flag = false;
+                break;
+            }
+        }
+        return Result.success("200",flag); // 判断文件名是否重复
+    }
+
+    @GetMapping("/inspectionOfIsCommon")
+    public Result inspectionOfIsCommon(@RequestParam("newname") String name){
+        QueryWrapper<CategoryEntity> wrapper = new QueryWrapper<>();
+        wrapper.eq("is_delete", 0);
+        wrapper.eq("is_common", 1);
+        List<CategoryEntity> list = categoryService.list(wrapper);
+        List<String>  nameList = new ArrayList<>();
+
+        for (CategoryEntity temp :list) {
+            nameList.add(temp.getLabel());
+        }
+        boolean flag = true;
+        for (String  tempName : nameList) {
+            if(tempName.equals(name)) {
+                flag = false;
+                break;
+            }
+        }
+        return Result.success("200",flag); // 判断文件名是否重复
+    }
+
+
+    //    zongqing新增疾病管理模块
+    @GetMapping("/category/getAllDisease")
+    public Result<List<CategoryEntity>> getAllDisease(){
+        List<CategoryEntity> list = categoryService.getAllDisease();
+        System.out.println(JSON.toJSONString(list));
+        return Result.success("200",list);
+    }
+    @PostMapping("/category/addCategory")
+    public Result addCategory(@RequestBody AddDiseaseVo addDiseaseVo){
+        return Result.success("200",categoryService.addCategory(addDiseaseVo));
+    }
+    @PostMapping("/category/updateCategory")
+    public Result updateCategory(@RequestBody UpdateDiseaseVo updateDiseaseVo){
+        return categoryService.updateCategory(updateDiseaseVo);
+
+//        return categoryService.updateById(categoryEntity)? Result.success("修改成功"):Result.fail("修改失败");
+    }
+    @PostMapping("/category/deleteCategory")
+    public Result deleteCategory(@RequestBody List<String> deleteIds){
+        System.out.println("删除");
+        System.out.println(deleteIds);
+        categoryService.removeCategorys(deleteIds);
         return Result.success("删除成功");
     }
 
