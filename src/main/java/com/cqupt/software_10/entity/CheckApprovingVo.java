56fa4316524658f24ad12c9393d08e04c4b7c3b0
wzj <zjwang_184@163.com>
2024-04-29 16:43:57 +0800

    管理员端数据管理添加下载审批功能

---
+++ src/main/java/com/cqupt/software_10/entity/CheckApprovingVo.java
@@ -0,0 +1,14 @@
+package com.cqupt.software_10.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckApprovingVo {
+    private String uid ;
+    private String username;
+    private  String classpath;
+}
